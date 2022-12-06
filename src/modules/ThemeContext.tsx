d4b9aa9f813bc920b8e9e7b4d1fccda1f6a41efc
rofireza <arrofirezasatria@gmail.com>
2022-12-06 07:27:18 +0700

    feat: folder utils, module components and hooks

--- src/modules/ThemeContext.tsx
+++ src/modules/ThemeContext.tsx
@@ -5,6 +5,7 @@ import {
 } from "@mui/material/styles";
 
 import { getDesignTokens } from "@modules/brandingTheme";
+import { useMediaQuery } from "@mui/material";
 
 export const ThemeContext = React.createContext({
   darkMode: {},
@@ -20,21 +21,29 @@ interface Props {
 }
 
 export default function ThemeProvider({ children }: Props) {
-  const [darkMode, setDarkMode] = React.useState<string | null>(null);
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const preferredMode = prefersDarkMode ? "dark" : "light";
+  const [darkMode, setDarkMode] = React.useState<"light" | "dark">(
+    localStorage.getItem("mui-dark-mode") === null ? preferredMode : "dark"
+  );
+
+  React.useEffect(() => {
+    let tempMode: string | null = "light";
+    if (typeof window !== "undefined") {
+      tempMode = localStorage.getItem("mui-dark-mode");
+    }
+
+    if (tempMode === ("light" || "dark")) {
+      console.log(tempMode + "inside useEffect");
+      setDarkMode(tempMode);
+      localStorage.setItem("mui-dark-mode", darkMode);
+    }
+  }, []);
 
   const toggleDarkMode = (): void => {
     setDarkMode((prev) => (prev === "dark" ? "light" : "dark"));
+    localStorage.setItem("mui-dark-mode", darkMode);
   };
-
-  React.useEffect(() => {
-    let initialMode = "light";
-    try {
-      initialMode = localStorage.getItem("dark-mode") || initialMode;
-    } catch (error) {
-      // do nothing
-    }
-    setDarkMode(initialMode);
-  }, []);
 
   const brandingDesignTokens = getDesignTokens(darkMode);
 
