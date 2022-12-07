7a4ab18a6d9c07579bf4b183429d384d0ffc2311
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2022-12-07 13:44:48 +0700

    refactor: back to basic themecontext without localstorage

--- src/modules/ThemeContext.tsx
+++ src/modules/ThemeContext.tsx
@@ -6,12 +6,9 @@ import {
 
 import { getDesignTokens } from "@modules/brandingTheme";
 import { useMediaQuery } from "@mui/material";
-// import useLocalStorage from "./hooks/useLocalStorage";
-
-import useLocalStorage from "beautiful-react-hooks/useLocalStorage";
 
 export const ThemeContext = React.createContext({
-  darkMode: {},
+  darkMode: "light",
   toggleDarkMode: () => console.log("You forgot to wrap with ThemeContext"),
 });
 
@@ -25,31 +22,15 @@ interface Props {
 
 export default function ThemeProvider({ children }: Props) {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const [darkMode, setDarkMode] = React.useState(
+  const [darkMode, setDarkMode] = React.useState<"light" | "dark">(
     prefersDarkMode === true ? "dark" : "light"
   );
 
-  React.useEffect(() => {
-    // console.log("useeffect");
-    if (typeof window !== "undefined") {
-      let initialMode = localStorage.getItem("mui-dark-mode");
-      if (initialMode !== null) {
-        console.log(darkMode);
-        setDarkMode(darkMode);
-        localStorage.setItem("mui-dark-mode", darkMode);
-        // console.log(initialMode);
-        // console.log(darkMode);
-      }
-    }
-  }, []);
-
   const toggleDarkMode = () => {
-    setDarkMode((prev: string) => (prev === "dark" ? "light" : "dark"));
-    localStorage.setItem("mui-dark-mode", darkMode);
+    setDarkMode((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
   const theme = React.useMemo(() => {
-    console.log(darkMode);
     const brandingDesignTokens = getDesignTokens(darkMode);
     let nextTheme = createTheme({
       ...brandingDesignTokens,
