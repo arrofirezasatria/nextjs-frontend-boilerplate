6277ea345b2bc9976297c586f1d4c9f0cd75e206
rofireza <arrofirezasatria@gmail.com>
2022-12-09 07:16:08 +0700

    feat: semantic nav link

--- src/components/AppsBar.tsx
+++ src/components/AppsBar.tsx
@@ -1,5 +1,6 @@
 import {
   AppBar,
+  Box,
   Button,
   Container,
   IconButton,
@@ -10,8 +11,16 @@ import React from "react";
 import { useThemeContext } from "@modules/components/ThemeContext";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Link from "next/link";
 
-const link = [{ title: "", link: "/ad" }, { title: "/asd" }];
+const link = [
+  { title: "about", link: "/about" },
+  { title: "blog", link: "/blog" },
+];
+
+const CustomLink = (params: { url: string; title: string }) => {
+  return <Link href={params.url}>{params.title}</Link>;
+};
 
 export default function AppsBar() {
   const { toggleDarkMode, darkMode } = useThemeContext();
@@ -20,7 +29,17 @@ export default function AppsBar() {
     <AppBar>
       <Container>
         <Toolbar>
-          <Typography>ad</Typography>
+          <Box component="nav" sx={{ display: "flex", flexGrow: 1, gap: 4 }}>
+            <ul>
+              {link.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <CustomLink url={item.link} title={item.title} />
+                  </li>
+                );
+              })}
+            </ul>
+          </Box>
           <IconButton onClick={toggleDarkMode}>
             {darkMode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
           </IconButton>
