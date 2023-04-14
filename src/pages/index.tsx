0915d2df6b527f5d9ac9a086f1ea855e893b443c
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2023-04-14 12:36:36 +0700

    chore: update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
             textAlign={"center"}
             sx={{ fontSize: "26px", fontWeight: "bold", fontFamily: "", my: 3 }}
           >
-            NEXTJS MUI BOILERPLATE
+            NEXTJS MUI BOILERPLATE - Template One
           </Typography>
           <Typography
             textAlign={"center"}
